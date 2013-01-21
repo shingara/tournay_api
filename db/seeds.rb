a6222998f6a50207e2fe76101fa37b9a3e12ac2b
Cyril Mougel <cyril.mougel@gmail.com>
2013-01-21 23:10:12 +0100

    add some new neighborhood card

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
 
+# Yellow card level 1
+
 NeighborhoodCard.create!({
   :cost => 1,
   :color => Color.new(:yellow),
@@ -64,3 +66,153 @@ NeighborhoodCard.create!({
     :buy => :citizen
   )
 })
+
+NeighborhoodCard.create!({
+  :cost => 1,
+  :color => Color.new(:yellow),
+  :victory_point => 1,
+  :level => 1,
+  :name => 'Carrière',
+  :card_type => 'building',
+  :action => CardAction.new(
+    :color_effect => nil,
+    :around => false,
+    :in_activate => false,
+    :in_place => false,
+    :on_people => false,
+    :on_building => false,
+    :win => true,
+    :denier => 4,
+    :cost => false,
+    :buy => :nothing
+  )
+})
+
+NeighborhoodCard.create!({
+  :cost => 1,
+  :color => Color.new(:yellow),
+  :victory_point => 0,
+  :level => 1,
+  :name => 'Ferme',
+  :card_type => 'building',
+  :action => CardAction.new(
+    :color_effect => nil,
+    :around => false,
+    :in_activate => false,
+    :in_place => false,
+    :on_people => false,
+    :on_building => false,
+    :win => true,
+    :denier => 1,
+    :cost => false,
+    :buy => :empty_neighborhood
+  )
+})
+
+NeighborhoodCard.create!({
+  :cost => 1,
+  :color => Color.new(:yellow),
+  :victory_point => 0,
+  :level => 1,
+  :name => 'Marché',
+  :card_type => 'building',
+  :action => CardAction.new(
+    :color_effect => nil,
+    :around => false,
+    :in_activate => false,
+    :in_place => false,
+    :on_people => false,
+    :on_building => false,
+    :win => true,
+    :denier => 5,
+    :cost => false,
+    :buy => :nothing
+  )
+})
+
+NeighborhoodCard.create!({
+  :cost => 1,
+  :color => Color.new(:yellow),
+  :victory_point => 1,
+  :level => 1,
+  :name => 'Compagnon',
+  :card_type => 'people',
+  :action => CardAction.new(
+    :color_effect => Color.new(:grey),
+    :around => true,
+    :in_activate => true,
+    :in_place => false,
+    :on_people => false,
+    :on_building => true,
+    :win => true,
+    :denier => 1,
+    :cost => false,
+    :buy => :nothing
+  )
+})
+
+NeighborhoodCard.create!({
+  :cost => 1,
+  :color => Color.new(:yellow),
+  :victory_point => 1,
+  :level => 1,
+  :name => 'Grange',
+  :card_type => 'building',
+  :action => CardAction.new(
+    :color_effect => Color.new(:yellow),
+    :around => false,
+    :in_activate => false,
+    :in_place => false,
+    :on_people => false,
+    :on_building => false,
+    :win => true,
+    :denier => 0,
+    :cost => false,
+    :buy => :one_card_level_2
+  )
+})
+
+NeighborhoodCard.create!({
+  :cost => 1,
+  :color => Color.new(:yellow),
+  :victory_point => 1,
+  :level => 1,
+  :name => 'Port',
+  :card_type => 'building',
+  :action => CardAction.new(
+    :color_effect => Color.new(:yellow),
+    :around => false,
+    :in_activate => false,
+    :in_place => false,
+    :on_people => false,
+    :on_building => false,
+    :win => true,
+    :denier => 0,
+    :cost => false,
+    :buy => :copy_building_not_activate
+  )
+})
+
+# White card level 1
+
+NeighborhoodCard.create!({
+  :cost => 1,
+  :color => Color.new(:white),
+  :victory_point => 1,
+  :level => 1,
+  :name => 'Bibliotheque',
+  :card_type => 'building',
+  :action => CardAction.new(
+    :color_effect => Color.new(:grey),
+    :around => false,
+    :in_activate => false,
+    :in_place => false,
+    :on_people => false,
+    :on_building => false,
+    :win => true,
+    :denier => 0,
+    :cost => false,
+    :buy => :one_card_level_2
+  )
+})
+
