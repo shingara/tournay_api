19b221df901b13d86e10b4f6510a457858c5ddea
Cyril Mougel <cyril.mougel@gmail.com>
2013-02-15 11:44:44 +0100

    avoid some failure in decorator spec with request.host sometimes define

--- spec/decorators/game_api_decorator_spec.rb
+++ spec/decorators/game_api_decorator_spec.rb
@@ -4,18 +4,19 @@ require 'spec_helper'
 describe GameApiDecorator do
   let(:game) { GameFabricator.new.create }
   let(:game_decorator) { GameApiDecorator.new(game) }
+  let(:host) { Draper::ViewContext.current.controller.request.host }
 
   describe "#links" do
     it 'have self link' do
-      expect(game_decorator.links['self']).to eq({'href' => "http://test.host/games/#{game.id}"})
+      expect(game_decorator.links['self']).to eq({'href' => "http://#{host}/games/#{game.id}"})
     end
 
     it 'have add_player link' do
-      expect(game_decorator.links['add_player']).to eq({'href' => "http://test.host/games/#{game.id}/add_player", 'method' => 'post', 'params' => ['name', 'id']})
+      expect(game_decorator.links['add_player']).to eq({'href' => "http://#{host}/games/#{game.id}/add_player", 'method' => 'post', 'params' => ['name', 'id']})
     end
 
     it 'have play link' do
-      expect(game_decorator.links['play']).to eq({'href' => "http://test.host/games/#{game.id}/play", 'method' => 'post', 'params' => ['player_id', 'action', 'color', 'level']})
+      expect(game_decorator.links['play']).to eq({'href' => "http://#{host}/games/#{game.id}/play", 'method' => 'post', 'params' => ['player_id', 'action', 'color', 'level']})
     end
   end
 
