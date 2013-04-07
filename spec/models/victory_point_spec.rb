0bbb8016306387ca182e31599ac1e29e2d7003f9
Cyril Mougel <cyril.mougel@gmail.com>
2013-04-07 17:46:32 +0200

    fix rspec failure after update mongoid-rspec 1.7.0

--- spec/models/victory_point_spec.rb
+++ spec/models/victory_point_spec.rb
@@ -6,7 +6,7 @@ describe VictoryPoint do
   it { should have_field(:other).of_type(Integer) }
   it { should have_field(:condition).of_type(String) }
 
-  it { should validate_inclusion_of(:condition).to_allow("triple_neighborhood_color") }
+  it { should validate_inclusion_of(:condition).to_allow(["triple_neighborhood_color"]) }
 
 
 end
